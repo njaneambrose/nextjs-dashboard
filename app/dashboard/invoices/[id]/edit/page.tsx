2c69043fe85ce38fe64ea8dfbdc57134187f25c6
Ambrose Njane <njaneambrose@gmail.com>
2024-08-18 19:37:58 +0300

    Fix form-bug

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,8 +1,9 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers , fetchInvoiceById } from '@/app/lib/data';
-import { notFound } from 'next/navigation';
- 
+import { notFound } from 'next/navigation'; 
+import { CustomerField } from '@/app/lib/definitions';
+
 export default async function Page({params}: { params: {id: string}}) {
   const id = params.id;  
   const [invoice, customers] = await Promise.all([
@@ -26,7 +27,7 @@ export default async function Page({params}: { params: {id: string}}) {
           },
         ]}
       />
-      <Form invoice={invoice} customers={customers} />
+      <Form invoice={invoice} customers={customers as CustomerField[]} />
     </main>
   );
 }
