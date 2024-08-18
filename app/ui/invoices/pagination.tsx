615f39a9f6f3948f521539910d1c58c6981da746
Ambrose Njane <njaneambrose@gmail.com>
2024-08-18 18:53:39 +0300

    fix typecasting bug

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -11,7 +11,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
 
   const pathname = usePathname();
   const searchParams = useSearchParams();
-  const currentPage = searchParams.get('page') || 1;
+  const currentPage = Number(searchParams.get('page')) || 1;
   const allPages = generatePagination(currentPage, totalPages);
 
   const createPageURL = (pageNumber : number | string) =>{
