d1b13ae28f029a6472e2fe76782bfec3a9f48213
Ambrose Njane <njaneambrose@gmail.com>
2024-08-19 11:58:44 +0300

    finished tutorial-not sure

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,7 +4,9 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
-
+import { signIn } from '@/auth'
+import { AuthError } from 'next-auth';
+ 
 const FormSchema = z.object({
     id: z.string(),
     customerId: z.string({invalid_type_error: 'Please select a customer'}),
@@ -25,6 +27,21 @@ export type State = {
 const CreateInvoice = FormSchema.omit({id: true, date: true});
 
 const UpdateInVoice = FormSchema.omit({id: true, date: true});
+
+export async function authenticate(prevState: string | undefined, formData : FormData){
+    try {
+        await signIn('credentials', formData);
+    } catch (error) {
+        if(error instanceof AuthError){
+            switch(error.type){
+                case 'CredentialsSignin' : return 'Invalid Credentials';
+                default:
+                    return "Something Went wrong";
+            }
+        }
+    }
+}
+
 
 export async function updateInvoice( id: string, prevState: State, formData: FormData){
     
